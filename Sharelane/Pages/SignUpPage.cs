55937f9641e60edd3a779d0477fd365b66b2d85e
Nikolajml <nicolas.maliavko@gmail.com>
2023-05-20 18:52:30 +0300

    Created three SignUpTests

---
+++ Sharelane/Pages/SignUpPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharelane.Pages
+{
+    internal class SignUpPage
+    {
+    }
+}
