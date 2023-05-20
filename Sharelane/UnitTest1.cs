e763b8921a17c4cd0f35daa5b337a94091bb5a45
Nikolajml <nicolas.maliavko@gmail.com>
2023-05-20 16:42:13 +0300

    Add project files.

---
+++ Sharelane/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Sharelane
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
